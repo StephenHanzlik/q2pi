80a858bc68ffc73cdc15811ee785398d4145b8b2
Christopher Grabski <grabc59@gmail.com>
2016-12-06 15:31:06 -0700

    add node modules

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 8000;
+
+app.use(morgan('short'));
+app.use(express.static(path.join('public')));
+
+app.listen(port, () => {
+    console.log('Listening on http://localhost:' + port);
+});
+
+module.exports = app;
