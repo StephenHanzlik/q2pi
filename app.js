6e388d203bdd3b666b7aebc615c6c8c33b283fbb
Christopher Grabski <grabc59@gmail.com>
2016-12-06 15:35:59 -0700

    adding npm knex, pg, body-parser to dependencies

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000;
 
 app.use(morgan('short'));
-app.use(express.static(path.join('public')));
+app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log('Listening on http://localhost:' + port);
