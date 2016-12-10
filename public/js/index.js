a6cf4a2b2732adfbbc78d6a16f64333ab5ffa29b
Stephen Hanzlik <stephenhanzlik@Stephens-MBP.lan>
2016-12-09 18:01:09 -0700

    seeded files table with more information to check how it displays on table

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,18 @@
 (function() {
   'use strict';
-<<<<<<< HEAD
-
   $.getJSON('/uploads')
     .done((files) => {
       // const $books = $('#books');
       // var fileObj = JSON.parse(files);
 
-      for (const file of files) {
+      for (let file of files) {
         var id = file.id;
         var name = file.name;
         var category = file.category;
         var user_id = file.user_id;
         var created_at = file.created_at;
 
-        $('#table tr:last').after('<th scope="row">' + id + '<td>' + name + '</td><td class="right">' + category + '</td><td>' + user_id + '</td><td>' + created_at + '</td></tr>');
+        $('#table tr:last').after('<tr><th scope="row">' + id + '<td>' + name + '</td><td class="right">' + category + '</td><td>' + user_id + '</td><td>' + created_at + '</td></tr>');
 
       }
     })
@@ -22,31 +20,3 @@
       console.log("Unable to retrieve files");
     });
 })();
-=======
-  $.getJSON('/upload')
-    .done((files) => {
-      const $fileData = $('#fileData');
-
-      for (const fileData of fileData) {
-        const $anchor = $('<a>')
-          .attr({
-            href: `/book.html?id=${book.id}`,
-            'data-delay': '50',
-            'data-tooltip': book.title
-          })
-          .tooltip();
-
-        const $card = $('<div>').addClass('card');
-        const $cardImage = $('<div>').addClass('card-image');
-        const $col = $('<div>').addClass('col s6 m4 l3');
-        const $img = $('<img>').attr({ src: book.coverUrl, alt: book.title });
-
-        $cardImage.append($img);
-        $anchor.append($cardImage);
-        $card.append($anchor);
-        $col.append($card);
-        $books.append($col);
-      }
-    })
-})
->>>>>>> de586add44f04032b92e3a4a0715208766431c7c
--- seeds/02_uploads.js
+++ seeds/02_uploads.js
@@ -2,22 +2,46 @@
 var path = require('path');
 
 exports.seed = function(knex, Promise) {
-    var uploadDir = path.join(__dirname, '../public/uploads');
-    // Deletes ALL existing entries
-    return knex('uploads').del()
-        .then(function() {
-            return Promise.all([
-                // Inserts seed entries
-                knex('uploads').insert({
-                    id: 1,
-                    name: 'seed1test.txt',
-                    path: uploadDir,
-                    category: 'text',
-                    user_id: 1,
-                    created_at: new Date('2016-06-26 14:26:16 UTC'),
-                    updated_at: new Date('2016-06-26 14:26:16 UTC')
-                }),
-                knex.raw("SELECT setval('uploads_id_seq', (SELECT MAX(id) FROM uploads))")
-            ]);
-        });
+  var uploadDir = path.join(__dirname, '../public/uploads');
+  // Deletes ALL existing entries
+  return knex('uploads').del()
+    .then(function() {
+      return Promise.all([
+        // Inserts seed entries
+        knex('uploads').insert([{
+          id: 1,
+          name: 'seed1test.txt',
+          path: uploadDir,
+          category: 'Text',
+          user_id: 1,
+          created_at: new Date('2016-06-26 14:26:16 UTC'),
+          updated_at: new Date('2016-06-26 14:26:16 UTC')
+        }, {
+          id: 2,
+          name: 'dinkydink.png',
+          path: uploadDir,
+          category: 'Image',
+          user_id: 1,
+          created_at: new Date('2016-06-26 14:26:16 UTC'),
+          updated_at: new Date('2016-06-26 14:26:16 UTC')
+        }, {
+          id: 3,
+          name: 'spider.mp4',
+          path: uploadDir,
+          category: 'Movie',
+          user_id: 1,
+          created_at: new Date('2016-06-26 14:26:16 UTC'),
+          updated_at: new Date('2016-06-26 14:26:16 UTC')
+        }, {
+          id: 4,
+          name: 'tron.mp4',
+          path: uploadDir,
+          category: 'Movie',
+          user_id: 1,
+          created_at: new Date('2016-06-26 14:26:16 UTC'),
+          updated_at: new Date('2016-06-26 14:26:16 UTC')
+        }]),
+        knex.raw("SELECT setval('uploads_id_seq', (SELECT MAX(id) FROM uploads))")
+      ]);
+    });
 };
