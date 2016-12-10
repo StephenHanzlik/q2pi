8af725e8ae85670b668ed783b43c5616df5826cd
Stephen Hanzlik <stephenhanzlik@Stephens-MBP.lan>
2016-12-09 17:21:44 -0700

    add index.js and logic to pull file data for table

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,23 @@
 (function() {
   'use strict';
-  $.getJSON('/upload')
+
+  $.getJSON('/uploads')
     .done((files) => {
-      const $fileData = $('#fileData');
+      // const $books = $('#books');
+      // var fileObj = JSON.parse(files);
 
-      for (const fileData of fileData) {
-        const $anchor = $('<a>')
-          .attr({
-            href: `/book.html?id=${book.id}`,
-            'data-delay': '50',
-            'data-tooltip': book.title
-          })
-          .tooltip();
+      for (const file of files) {
+        var id = file.id;
+        var name = file.name;
+        var category = file.category;
+        var user_id = file.user_id;
+        var created_at = file.created_at;
 
-        const $card = $('<div>').addClass('card');
-        const $cardImage = $('<div>').addClass('card-image');
-        const $col = $('<div>').addClass('col s6 m4 l3');
-        const $img = $('<img>').attr({ src: book.coverUrl, alt: book.title });
+        $('#table tr:last').after('<th scope="row">' + id + '<td>' + name + '</td><td class="right">' + category + '</td><td>' + user_id + '</td><td>' + created_at + '</td></tr>');
 
-        $cardImage.append($img);
-        $anchor.append($cardImage);
-        $card.append($anchor);
-        $col.append($card);
-        $books.append($col);
       }
     })
-})
+    .fail(() => {
+      console.log("Unable to retrieve files");
+    });
+})();
