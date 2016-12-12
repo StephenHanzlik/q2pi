eb5745adf1d308f06cd3ce9ce95a2795b39195ea
Stephen Hanzlik <stephenhanzlik@gmail.com>
2016-12-12 16:22:08 -0700

    opacity styling on contributors and changed redirect speed to 1700

--- public/js/upload.js
+++ public/js/upload.js
@@ -56,7 +56,7 @@ $('#upload-input').on('change', function(){
               $('.progress-bar').html('Done');
               window.setTimeout( function(){
                 window.location.href = "http://localhost:8000/user-landing.html";
-              }, 3000);
+              }, 1700);
               }
 
             }
