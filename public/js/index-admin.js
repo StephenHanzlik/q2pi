9ecd50617cddcd56c36409d8424d69be6c5990f1
Stephen Hanzlik <stephenhanzlik@gmail.com>
2016-12-13 12:29:37 -0700

    created seperate admin interface for deleting from special user landings page

---
+++ public/js/index-admin.js
@@ -0,0 +1,36 @@
+'use strict';
+(function() {
+  $.getJSON('/uploads')
+    .done((files) => {
+      for (let file of files) {
+        var name = file.name;
+        var username = file.username;
+        var created_at = file.created_at;
+        // TODO: change category to the download_path, category is temporarily being used as the download path for client's 'file download' links
+        var path = file.category;
+        $('#table tr:last').after('<tr><td><a href="' + path + '" download>' + name + '</a></td><td>' + username + '</td><td>' + created_at + '</td><td class="delete-button">x</td></tr>');
+      }
+      $('.delete-button').on('click', function(){
+        console.log(event.target);
+        console.log("clicked");
+      });
+    })
+    .fail(() => {
+      console.log("Unable to retrieve files");
+    });
+})();
+
+ //Needs work:  
+ var name = file.name;
+ var username = file.username;
+ var created_at = file.created_at;
+ const options = {
+   contentType: 'application/json',
+   data: JSON.stringify({ name, username, created_at }),
+   type: 'DELETE',
+   url: '/uploads'
+ };
+ $.ajax(options) {
+
+ }
+}):
