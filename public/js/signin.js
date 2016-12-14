8e395a2803bd0946264aca0d8d243a4320d3417d
Christopher Grabski <grabc59@gmail.com>
2016-12-14 12:23:26 -0700

    fix upload-route merge conflict with landing ajax call

--- public/js/signin.js
+++ public/js/signin.js
@@ -26,17 +26,6 @@
       url: '/token',
       email: email
     };
-<<<<<<< HEAD
-        $.ajax(options)
-        .done(() => {
-          window.location.href = '/user-landing.html';
-        })
-        .fail(($xhr) => {
-          toastr.error($xhr.responseText);
-          console.log("sign in fail");
-        });
-=======
-
     $.ajax(options)
       .done(() => {
         // window.location.href = '/user-landing.html';
@@ -46,6 +35,5 @@
         toastr.error($xhr.responseText);
         console.log("sign in fail");
       });
->>>>>>> 713f940c01990b6a66e9cf7dcbad2018c9da5d9d
   });
 })();
