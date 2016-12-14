6e77f044c4cf94b4d2b1475e46ec995f7daba3e8
Christopher Grabski <grabc59@gmail.com>
2016-12-14 12:15:28 -0700

    bring app.js back up to date, pasting in correct code with admin redirect

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const port = process.env.PORT || 8000;
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const privateKey = 'my_awesome_cookie_signing_key';
+var http = require('http');
 var path = require('path');
-var serialio_route = require('./routes/serialport-route');
+
 app.use(cookieParser());
 
 const authorize = function(req, res, next) {
@@ -30,11 +31,55 @@ const authorize = function(req, res, next) {
   }
 };
 
-app.get('/landing', authorize, function (req, res, next) {
-  console.log('*******************authorized');
-  
-  res.sendFile(path.join(__dirname + '/public/user-landing.html'));
+// app.get('/serialport', function() {
+//   console.log('serialport get log **************');
+// });
+
+
+// const express = require('express');
+// const router = express.Router();
+const arduino = require('./devices/arduino');
+
+app.get('/serialport/:event/:data', function(request, res) {
+  console.log("f:sendToSerial");
+  // get the parameters from the URL:
+  var eventAndCommand = request.params.event + request.params.data;
+  console.log("received "+ eventAndCommand);
+  // xmit to serial port close connection
+  arduino.myPort.write(eventAndCommand);
 });
+
+app.get('/landing', authorize, function (req, res, next) {
+  console.log(req.token);
+
+  var options = {
+    host: 'eggnogg',
+    port: '8000',
+    // path: '/serialport/' + 'Log in: ' + '/' + req.token
+    path: '/serialport/Log%20in:%20/' + req.token
+  };
+  var callback = function(response) {
+    var str = '';
+
+    //another chunk of data has been recieved, so append it to `str`
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+
+    //the whole response has been recieved, so we just print it out here
+    response.on('end', function () {
+      console.log(str);
+    });
+  };
+  http.request(options, callback).end();
+
+  if (req.token === 'dinkydinky@gmail.com') {
+    res.sendFile(path.join(__dirname + '/public/user-landing-admin.html'));
+  } else {
+    res.sendFile(path.join(__dirname + '/public/user-landing.html'));
+  }
+});
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -44,15 +89,13 @@ app.use(express.static('public'));
 var users = require('./routes/users.js');
 var uploads = require('./routes/upload-route.js');
 var token = require('./routes/token-route.js');
+// var serialio_route = require('./routes/serialport-route.js');
 
 
-app.use('/serialport', serialio_route);
+// app.use('/serialport', serialio_route);
 app.use('/users', users);
 app.use('/uploads', uploads);
 app.use('/token', token);
-
-
-app.use('/serialport', serialio_route);
 
 app.use((err, _req, res, _next) => {
   if (err.output && err.output.statusCode) {
