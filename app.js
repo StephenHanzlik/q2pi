405f226bd07c2124d136ff5a03b72ae5895657c5
Christopher Grabski <grabc59@gmail.com>
2016-12-13 22:24:08 -0700

    make the arduino requests a get request in app.js

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const cookieParser = require('cookie-parser');
 const privateKey = 'my_awesome_cookie_signing_key';
 var http = require('http');
 var path = require('path');
-var serialio_route = require('./routes/serialport-route');
 
 app.use(cookieParser());
 
@@ -31,6 +30,24 @@ const authorize = function(req, res, next) {
     return res.redirect('/signin.html');
   }
 };
+
+// app.get('/serialport', function() {
+//   console.log('serialport get log **************');
+// });
+
+
+// const express = require('express');
+// const router = express.Router();
+const arduino = require('./devices/arduino');
+
+app.get('/serialport/:event/:data', function(request, res) {
+  console.log("f:sendToSerial");
+  // get the parameters from the URL:
+  var eventAndCommand = request.params.event + request.params.data;
+  console.log("received "+ eventAndCommand);
+  // xmit to serial port close connection
+  arduino.myPort.write(eventAndCommand);
+});
 
 app.get('/landing', authorize, function (req, res, next) {
   console.log(req.token);
@@ -62,6 +79,7 @@ app.get('/landing', authorize, function (req, res, next) {
   }
 });
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(morgan('short'));
@@ -70,8 +88,10 @@ app.use(express.static('public'));
 var users = require('./routes/users.js');
 var uploads = require('./routes/upload-route.js');
 var token = require('./routes/token-route.js');
+// var serialio_route = require('./routes/serialport-route.js');
 
-app.use('/serialport', serialio_route);
+
+// app.use('/serialport', serialio_route);
 app.use('/users', users);
 app.use('/uploads', uploads);
 app.use('/token', token);
